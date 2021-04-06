01330c1a04ea1e92edb9d8dc1db681fd0d8e9e89
Behnam Ousat <behnam.ousat@gmail.com>
2021-04-06 10:49:39 -0600

    add sample credentials

---
+++ src/credentials.ts
@@ -0,0 +1,9 @@
+export default {
+    "db": {
+        "host": "YOUR_HOST",
+        "port": 12345,
+        "username": "DB_USERNAME",
+        "password": "DB_PASSWORD",
+        "database": "DB_NAME"
+    }
+} as const;
