6fac5c7d2b5bc0a39dc71d4ba47ef207fcbd2296
Behnam <60201681+behnam-o@users.noreply.github.com>
2021-04-06 10:52:32 -0600

    Delete credentials.ts

--- src/credentials.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    "db": {
-        "host": "YOUR_HOST",
-        "port": 12345,
-        "username": "DB_USERNAME",
-        "password": "DB_PASSWORD",
-        "database": "DB_NAME"
-    }
-} as const;
