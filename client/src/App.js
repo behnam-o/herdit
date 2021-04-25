2f1335dee33cf627d07ec4d9181f5982949236d3
Behnam Ousat <behnam.ousat@gmail.com>
2021-04-25 17:59:39 -0600

    add authentication

--- client/src/App.js
+++ client/src/App.js
@@ -8,16 +8,21 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import MenuBar from './components/MenuBar';
+import AuthRoute from './utils/authRoute';
+import { AuthProvider } from './context/auth';
+
 function App() {
    return (
-      <Router>
-         <Container>
-            <MenuBar />
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-         </Container>
-      </Router>
+      <AuthProvider>
+         <Router>
+            <Container>
+               <MenuBar />
+               <Route exact path="/" component={Home} />
+               <AuthRoute exact path="/login" component={Login} />
+               <AuthRoute exact path="/register" component={Register} />
+            </Container>
+         </Router>
+      </AuthProvider>
    );
 }
 
